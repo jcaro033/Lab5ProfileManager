27652df4eb0a16e5d240e8f0f60eb53681db6fdd
William Bickerton <wbick096@uottawa.ca>
2025-11-17 22:00:14 -0500

    Added onActivityResults(), errors in variable names

--- app/src/main/java/com/example/profilemanager/MainActivity.java
+++ app/src/main/java/com/example/profilemanager/MainActivity.java
@@ -60,7 +60,43 @@ public class MainActivity extends AppCompatActivity {
                 public void onActivityResult(ActivityResult result) {
                     if (result.getResultCode() == Activity.RESULT_OK) {
                         Intent data = result.getData();
-                        //CODE HERE
+                        ImageView avatarImage = (ImageView) findViewById(R.id.avatarImage);
+
+                        String drawableName = "flag_02";
+                        switch(data.getIntExtra("imageID", R.id.flagid00)){
+                            case R.id.flagid00:
+                                drawableName = "flag_00";
+                                break;
+                            case R.id.flag01:
+                                drawableName = "flag_01";
+                                break;
+                            case R.id.flag02:
+                                drawableName = "flag_02";
+                                break;
+                            case R.id.flag03:
+                                drawableName = "flag_03";
+                                break;
+                            case R.id.flag04:
+                                drawableName = "flag_04";
+                                break;
+                            case R.id.flag05:
+                                drawableName = "flag_05";
+                                break;
+                            case R.id.flag06:
+                                drawableName = "flag_06";
+                                break;
+                            case R.id.flag07:
+                                drawableName = "flag_07";
+                                break;
+                            case R.id.flag08:
+                                drawableName = "flag_08";
+                                break;
+                            case R.id.flag09:
+                                drawableName = "flag_09";
+                                break;
+                        }
+                        int resID = getResources().getIdentifier(drawableName, "drawable", getPackageName());
+                        avatarImage.setImageResource(resID);
 
 
                         //Tried to do example from lab video
